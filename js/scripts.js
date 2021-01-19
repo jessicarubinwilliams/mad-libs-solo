2f797f8bb75e1a55d2315896ba7af423a95b16d5
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-19 15:24:07 -0800

    Refactor JavaScript to use loops to grab user input and output into the story.
    Co-Authored-By: Mikey Kriegel <mikkrieg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    event.preventDefault();
+    // const person1 = $("input#person1").val();
+    // const person2 = $("input#person2").val();
+    // const animal = $("input#animal").val();
+    // const exclamation = $("input#exclamation").val();
+    // const verb = $("input#verb").val();
+    // const noun = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+
+    const madLibArray = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+
+
+    madLibArray.forEach(function(lib){
+      let input = $('input#' + lib).val();
+      $('span.' + lib).text(input);
+    });
+
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
